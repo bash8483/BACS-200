a236a3c1c2f252075ecac6bb029394cd93165aa5
bash8483 <60244828+bash8483@users.noreply.github.com>
2020-02-11 09:40:01 -0700

    new

---
+++ bacs200/01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<h1>Project #1</h2>
+
+    <h2>New literacies</h2>
+
+    <p>The new literacies of today are the ability to perform digital literacy. Today digital literacy is creating gateways for real time international communication. For example, a business that has international and domestic employees has access to video conferencing that happens in real time using a variety of applications. These applications include Zoom, Skype, and Google Hangouts; each one has the tools for video group calls. Another popular example is YouTube, this website has a variety of tools that allow for online communication and sharing. A tool such as Live Stream allows for a YouTube user to post a live video that can be seen by anyone in the world. YouTube is also home to a massive platform of user made videos that have a wide variety of content.Amazon is taking shopping to another level with their brand-new store in Los Angeles, in this store purchases are all made digitally with virtually no need for cashiers. As soon as you walk into the store you are identified by your Amazon members account. Whatever items you choose to buy, will be charged  to your account once you leave the store.Google has introduced many of today’s digital platforms that have already been intergraded into our education system. Google has also provided the necessary search engine needed to fuel the demands of digital communication. When we look to the future of digital literacy, Autonomous vehicles are on the horizon. Google produced the first concept design, other companies have joined into the design, such as Lyft and Tesla.</p>
+</body>
+
+</html>
